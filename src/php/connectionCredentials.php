99c87679d73240ca52a9dad877aa5bfc4d161fa8
kapitanTurek <kap.turekpp@gmail.com>
2024-08-01 20:48:12 +0200

    Exchanging all data

--- src/php/connectionCredentials.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $serverLocation = "localhost:3306";
-    $loginName = "root";
-    $passwordToAccess = "AdministratorPass";
-    $databaseName = "quizesDB";
-?>
--- src/php/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    session_destroy();
-    header("Location: adminLogin.php");
-    exit();
-?>
